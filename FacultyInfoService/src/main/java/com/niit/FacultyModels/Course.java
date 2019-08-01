0d2c99eb1ed4556dbd74b975f5e9bf51671065a8
Karenjeba <40361431+Karenjeba@users.noreply.github.com>
2019-08-01 22:51:39 +0530

    Create Course.java

---
+++ FacultyInfoService/src/main/java/com/niit/FacultyModels/Course.java
@@ -0,0 +1,33 @@
+package com.niit.FacultyModels;
+
+public class Course 
+{
+private String courseId;
+private String courseName;
+public Course()
+{
+	
+}
+public Course(String courseId, String courseName) {
+	super();
+	this.courseId = courseId;
+	this.courseName = courseName;
+}
+public String getCourseId() {
+	return courseId;
+}
+public void setCourseId(String courseId) {
+	this.courseId = courseId;
+}
+public String getCourseName() {
+	return courseName;
+}
+public void setCourseName(String courseName) {
+	this.courseName = courseName;
+}
+@Override
+public String toString() {
+	return "Course [courseId=" + courseId + ", courseName=" + courseName + "]";
+}
+
+}
