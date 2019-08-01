c2e3b96b27d4e9ebefc609c14e5c74078796e59b
Karenjeba <40361431+Karenjeba@users.noreply.github.com>
2019-08-01 22:51:34 +0530

    Create FacultyInfoServiceApplication.java

---
+++ FacultyInfoService/src/main/java/com/niit/FacultyInfoServiceApplication.java
@@ -0,0 +1,20 @@
+package com.niit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class FacultyInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyInfoServiceApplication.class, args);
+	}
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
+
+}
